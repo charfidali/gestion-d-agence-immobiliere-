7f7abd149da960c6b881102fec179f0f1f258593
Sami BEN FADHL <samibenfadhl@gmail.com>
2023-03-06 20:32:38 +0100

    changes stomp

--- Phase-Spring/EstateMarketPi/src/main/java/pi/app/estatemarket/websocket/ChatConfig.java
+++ Phase-Spring/EstateMarketPi/src/main/java/pi/app/estatemarket/websocket/ChatConfig.java
@@ -13,7 +13,7 @@ public class ChatConfig implements WebSocketMessageBrokerConfigurer {
 
 	@Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        //registry.addEndpoint("/ws").withSockJS();
+        registry.addEndpoint("/ws").withSockJS();
 
     }
 
