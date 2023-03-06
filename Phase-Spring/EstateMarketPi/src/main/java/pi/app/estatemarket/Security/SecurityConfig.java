b277309ce0eeeb1df6d4c9a2f983e60548ea3b58
Sami BEN FADHL <samibenfadhl@gmail.com>
2023-03-06 18:22:42 +0100

    fix sec

--- Phase-Spring/EstateMarketPi/src/main/java/pi/app/estatemarket/Security/SecurityConfig.java
+++ Phase-Spring/EstateMarketPi/src/main/java/pi/app/estatemarket/Security/SecurityConfig.java
@@ -68,7 +68,11 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
             "/UpdateComment/**",
             "/retrieveComment/**",
             "/DeleteComment/**",
-            "/ajouterEtAffecterCommentaireAUserEtCommentaire/**"
+            "/ajouterEtAffecterCommentaireAUserEtCommentaire/**",
+            "/reportComment/**",
+            "/api/contract/**",
+            "/pdf",
+            "/api/payment/**"
 
 
             // other public endpoints of your API may be appended to this array
